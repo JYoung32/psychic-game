af740510becdfab8dfbd713d8e915fea0603d7ea
Joseph Young <joseph.young32@yahoo.com>
2019-09-06 21:59:01 -0400

    edited JS to not reset all the time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,26 +7,33 @@ var losses = 0;
 var numberGuess = 9;
 //make variable string for guess choices
 var guessChoices = [];
+//have computer choose a random letter from the computerChoice variable
+var compGuess ;
+function computerGuess () {compGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(compGuess);
+return compGuess}
+//when window loads, fires computerGuess function
+window.onload = computerGuess();
 //add onkeyup function to record key pressed
 document.onkeyup = function(event) {
     //make a variable to store user's guess
     var userGuess = event.key;
-    //have computer choose a random letter from the computerChoice variable
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-    console.log(`${computerGuess} ; shows guess works`);
+    
+    //console.log(`${computerGuess} ; shows guess works`);
     //make a variable with users possible choices
     var userChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     //create if conditions for the game
     //if the user makes a guess run code, if false do nothing
     if (userChoices.indexOf(userGuess) > -1) {
         //if the users guess is the computers guess, user wins
-        if (userGuess === computerGuess) {
+        if (userGuess === compGuess) {
             wins++;
             numberGuess = 9;
             guessChoices = [];
+            computerGuess();
         }
         //if the user guess is not the computers guess, add user guess to guess choices array
-        if (userGuess != computerGuess) {
+        if (userGuess != compGuess) {
             numberGuess --;
             guessChoices.push(userGuess);
         }
@@ -35,6 +42,7 @@ document.onkeyup = function(event) {
             numberGuess = 9;
             losses ++;
             guessChoices = [];
+            computerGuess();
         }
     //change variables on the html document  
     document.getElementById("wins").innerHTML = "Wins: " + wins + "";
